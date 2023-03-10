7961690a4c1086ee4340d09aba2230541098d1ee
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-03-10 03:46:00 -0300

    Add external font

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Texto\nSegunda linha',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 50,
-          height: 1.0, // afastamento das linhas
-          fontStyle: FontStyle.italic,
-          fontWeight: FontWeight.w200,
-          color: Colors.blue.shade400,
-          backgroundColor: Colors.yellow.shade200,
-          decoration: TextDecoration.lineThrough, // inserir linhas
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Texto\nSegunda linha',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 50,
+            height: 1.0, // afastamento das linhas
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.w200,
+            color: Colors.blue.shade400,
+            backgroundColor: Colors.yellow.shade200,
+            decoration: TextDecoration.lineThrough, // inserir linhas
+          ),
         ),
-      ),
+        const Text(
+          'Fonte',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'RubikIso',
+            fontSize: 100,
+          ),
+        )
+      ],
     );
   }
 }
